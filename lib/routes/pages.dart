a747efe8483db98ee4d38d42767d9143b35a6811
Kauê Murakami <kauetmurakami@gmail.com>
2021-10-03 12:10:11 -0300

    feat: inicial updates

---
+++ lib/routes/pages.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+part './routes.dart';
+
+abstract class AppPages {
+  static final pages = [
+    //GetPage(name: Routes.HOME, page:()=> MyHomePage(),)
+  ];
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,8 @@
+part of './pages.dart';
+
+abstract class Routes {
+  static const auth = '/';
+  static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const CADASTRO = '/cadastro';
+}
