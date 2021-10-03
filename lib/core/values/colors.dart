17107c829ad534f6d8c66b1c8145de5c813058f5
Kauê Murakami <kauetmurakami@gmail.com>
2021-10-03 12:12:46 -0300

    feat: add initial values

---
+++ lib/core/values/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/painting.dart';
+
+const light_blue = Color(0xff4A88D9);
+const blue = Color(0xff4184BF);
+const light_red = Color(0xff4184BF);
+const red = Color(0xff4184BF);
+const my_black = Color(0xff4184BF);
---
+++ lib/core/values/strings.dart
@@ -0,0 +1,1 @@
+const app_name = 'Democratize';
